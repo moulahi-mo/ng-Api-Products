b838d9afc722b5359156318a3a4d25de64e1c54b
moulahi-mo <moulahi.mo@gmail.com>
2021-02-28 19:52:37 +0100

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,14 @@ import { StoreModule } from '@ngrx/store';
 import { MaterialModule } from './shared/material/material.module';
 import { environment } from '../environments/environment';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { LoaderComponent } from './components/shared/loader/loader.component';
+import { ErrorComponent } from './components/shared/error/error.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, NavbarComponent, SidebarComponent, AuthComponent, LoaderComponent, ErrorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
